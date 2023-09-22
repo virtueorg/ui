8c3debce9cddf4f797ac09ae599b8c77e0a964bd
Leonardo <leonardodipace@gmail.com>
2023-09-22 22:18:52 +0200

    Run prettier

--- src/lib/components/Form/FormFields.svelte
+++ src/lib/components/Form/FormFields.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { cn } from "$lib/utils/misc.js";
+  import { cn } from "$lib/utils/misc";
   import type { HTMLAttributes } from "svelte/elements";
 
   type $$Props = HTMLAttributes<HTMLDivElement>;
