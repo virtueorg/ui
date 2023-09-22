f36a4f3f20b0d84fd3efe8ac9d431975d86d3122
Leonardo <leonardodipace@gmail.com>
2023-09-22 16:39:15 +0200

    Added initial icons

---
+++ src/lib/icons/DotIcon.svelte
@@ -0,0 +1,10 @@
+<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12Z"
+    fill="currentColor"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
---
+++ src/lib/icons/index.ts
@@ -0,0 +1,9 @@
+import ChevronDownIcon from "./ChevronDownIcon.svelte";
+import ChevronRightIcon from "./ChevronRightIcon.svelte";
+import DotIcon from "./DotIcon.svelte";
+import PenIcon from "./PenIcon.svelte";
+import TickIcon from "./TickIcon.svelte";
+import TrashIcon from "./TrashIcon.svelte";
+import UserIcon from "./UserIcon.svelte";
+
+export { ChevronDownIcon, ChevronRightIcon, DotIcon, PenIcon, TickIcon, TrashIcon, UserIcon };
