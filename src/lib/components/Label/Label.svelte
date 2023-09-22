a9597440291a47b745ae9a4d189942e34b43873e
Leonardo <leonardodipace@gmail.com>
2023-09-22 16:47:05 +0200

    Added Label component

---
+++ src/lib/components/Label/Label.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/misc";
+  import { Label } from "bits-ui";
+
+  type $$Props = Label.Props;
+
+  export { className as class };
+
+  let className = "";
+</script>
+
+<Label.Root class={cn("text-sm", "text-mutedForeground", className)} {...$$restProps}>
+  <slot />
+</Label.Root>
