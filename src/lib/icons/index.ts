7cda257797ecab184ace6afe13e67f719b2f5c23
Leonardo <leonardodipace@gmail.com>
2023-09-23 02:29:31 +0200

    Added SpinnerIcon

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -2,8 +2,18 @@ import ChevronDownIcon from "./ChevronDownIcon.svelte";
 import ChevronRightIcon from "./ChevronRightIcon.svelte";
 import DotIcon from "./DotIcon.svelte";
 import PenIcon from "./PenIcon.svelte";
+import SpinnerIcon from "./SpinnerIcon.svelte";
 import TickIcon from "./TickIcon.svelte";
 import TrashIcon from "./TrashIcon.svelte";
 import UserIcon from "./UserIcon.svelte";
 
-export { ChevronDownIcon, ChevronRightIcon, DotIcon, PenIcon, TickIcon, TrashIcon, UserIcon };
+export {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  DotIcon,
+  PenIcon,
+  SpinnerIcon,
+  TickIcon,
+  TrashIcon,
+  UserIcon,
+};
