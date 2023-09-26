001de20418abb342d1042bad2542f3814f4bce83
Leonardo <leonardodipace@gmail.com>
2023-09-26 16:24:33 +0200

    Updated Input design

--- src/lib/components/Input/InputLabel.svelte
+++ src/lib/components/Input/InputLabel.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <label
-  class={cn("absolute", "top-0", "left-0", "p-3", "text-sm", "text-mutedForeground", className)}
+  class={cn("absolute", "top-0", "left-0", "p-3", "text-sm", "text-muted", className)}
   {...$$restProps}
 >
   <slot />
