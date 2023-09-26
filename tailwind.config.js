567f3ba1e623293c0fe75a7b46cc016bff08ffc0
Leonardo <leonardodipace@gmail.com>
2023-09-26 15:53:36 +0200

    Simplified colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,10 @@ export default {
       colors: {
         background: "#020617",
         foreground: "#f8fafc",
-
-        muted: "#0f172a",
-        mutedForeground: "#94a3b8",
-        mutedHover: "#1e293b",
-
+        muted: "#94a3b8",
         primary: "#818cf8",
-        primaryForeground: "#020617",
-        primaryHover: "#6366f1",
-
         warning: "#fbbf24",
-        warningForeground: "#020617",
-        warningHover: "#f59e0b",
-
         error: "#f87171",
-        errorForeground: "#020617",
-        errorHover: "#ef4444",
       },
     },
   },
