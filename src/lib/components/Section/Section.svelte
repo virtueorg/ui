cc99ec9acbe72a2ef49494c813758535dae382d3
Leonardo <leonardodipace@gmail.com>
2023-09-28 09:38:59 +0200

    Added Section component

---
+++ src/lib/components/Section/Section.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/misc";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type $$Props = HTMLAttributes<HTMLElement>;
+
+  export { className as class };
+
+  let className = "";
+</script>
+
+<section class={cn("flex", "flex-col", "gap-2", className)} {...$$restProps}>
+  <slot />
+</section>
