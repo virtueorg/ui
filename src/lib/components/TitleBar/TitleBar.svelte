3b60bbce44e500c2525706efc4c094a2d9325bdc
Leonardo <leonardodipace@gmail.com>
2023-09-28 10:14:54 +0200

    Added TitleBar component

---
+++ src/lib/components/TitleBar/TitleBar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/misc";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type $$Props = HTMLAttributes<HTMLDivElement>;
+
+  export { className as class };
+
+  let className = "";
+</script>
+
+<div class={cn("flex", "items-center", "gap-2", className)} {...$$restProps}>
+  <slot />
+</div>
---
+++ src/lib/components/TitleBar/index.ts
@@ -0,0 +1,7 @@
+import TitleBar from "./TitleBar.svelte";
+import TitleBarDescription from "./TitleBarDescription.svelte";
+import TitleBarHeading from "./TitleBarHeading.svelte";
+import TitleBarImage from "./TitleBarImage.svelte";
+import TitleBarTitle from "./TitleBarTitle.svelte";
+
+export { TitleBar, TitleBarDescription, TitleBarHeading, TitleBarImage, TitleBarTitle };
