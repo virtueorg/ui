f9570b1377c66bc53141ef32bf55bcae522b61d8
Leonardo <leonardodipace@gmail.com>
2023-09-29 17:47:37 +0200

    Exported icons

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,21 +1,43 @@
+import BellIcon from "./BellIcon.svelte";
+import BookIcon from "./BookIcon.svelte";
+import BoxIcon from "./BoxIcon.svelte";
+import CalendarIcon from "./CalendarIcon.svelte";
 import ChevronDownIcon from "./ChevronDownIcon.svelte";
 import ChevronRightIcon from "./ChevronRightIcon.svelte";
 import CloseIcon from "./CloseIcon.svelte";
 import DotIcon from "./DotIcon.svelte";
+import EditIcon from "./EditIcon.svelte";
+import EmailIcon from "./EmailIcon.svelte";
+import EuroIcon from "./EuroIcon.svelte";
+import FolderIcon from "./FolderIcon.svelte";
+import GridIcon from "./GridIcon.svelte";
 import PenIcon from "./PenIcon.svelte";
+import ShieldIcon from "./ShieldIcon.svelte";
 import SpinnerIcon from "./SpinnerIcon.svelte";
 import TickIcon from "./TickIcon.svelte";
 import TrashIcon from "./TrashIcon.svelte";
 import UserIcon from "./UserIcon.svelte";
+import UsersIcon from "./UsersIcon.svelte";
 
 export {
+  BellIcon,
+  BookIcon,
+  BoxIcon,
+  CalendarIcon,
   ChevronDownIcon,
   ChevronRightIcon,
   CloseIcon,
   DotIcon,
+  EditIcon,
+  EmailIcon,
+  EuroIcon,
+  FolderIcon,
+  GridIcon,
   PenIcon,
+  ShieldIcon,
   SpinnerIcon,
   TickIcon,
   TrashIcon,
   UserIcon,
+  UsersIcon,
 };
