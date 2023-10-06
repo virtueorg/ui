44060bb6468635b694b52fe690c57c28c3ed998b
Leonardo <leonardodipace@gmail.com>
2023-10-06 19:46:29 +0200

    Exported icons

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,53 +1,89 @@
+import ArchiveIcon from "./ArchiveIcon.svelte";
 import BellIcon from "./BellIcon.svelte";
 import BookIcon from "./BookIcon.svelte";
+import BookmarkIcon from "./BookmarkIcon.svelte";
 import BoxIcon from "./BoxIcon.svelte";
 import BuildingIcon from "./BuildingIcon.svelte";
 import CalendarIcon from "./CalendarIcon.svelte";
 import ChevronDownIcon from "./ChevronDownIcon.svelte";
 import ChevronRightIcon from "./ChevronRightIcon.svelte";
 import CloseIcon from "./CloseIcon.svelte";
+import CloudIcon from "./CloudIcon.svelte";
 import DotIcon from "./DotIcon.svelte";
+import DownloadIcon from "./DownloadIcon.svelte";
 import EditIcon from "./EditIcon.svelte";
 import EmailIcon from "./EmailIcon.svelte";
 import ErrorIcon from "./ErrorIcon.svelte";
 import EuroIcon from "./EuroIcon.svelte";
+import EyeIcon from "./EyeIcon.svelte";
+import EyeSlashIcon from "./EyeSlashIcon.svelte";
+import FilterIcon from "./FilterIcon.svelte";
 import FolderIcon from "./FolderIcon.svelte";
 import GridIcon from "./GridIcon.svelte";
+import HeartIcon from "./HeartIcon.svelte";
+import HouseIcon from "./HouseIcon.svelte";
 import InfoIcon from "./InfoIcon.svelte";
 import LayersIcon from "./LayersIcon.svelte";
+import LoginIcon from "./LoginIcon.svelte";
+import LogoutIcon from "./LogoutIcon.svelte";
+import MenuIcon from "./MenuIcon.svelte";
 import PenIcon from "./PenIcon.svelte";
+import PlusIcon from "./PlusIcon.svelte";
+import SearchIcon from "./SearchIcon.svelte";
+import SettingsIcon from "./SettingsIcon.svelte";
 import ShieldIcon from "./ShieldIcon.svelte";
+import SortIcon from "./SortIcon.svelte";
 import SpinnerIcon from "./SpinnerIcon.svelte";
 import TickIcon from "./TickIcon.svelte";
 import TrashIcon from "./TrashIcon.svelte";
+import UploadIcon from "./UploadIcon.svelte";
 import UserIcon from "./UserIcon.svelte";
 import UsersIcon from "./UsersIcon.svelte";
+import VerifiedIcon from "./VerifiedIcon.svelte";
 import WarningIcon from "./WarningIcon.svelte";
 
 export {
+  ArchiveIcon,
   BellIcon,
   BookIcon,
+  BookmarkIcon,
   BoxIcon,
   BuildingIcon,
   CalendarIcon,
   ChevronDownIcon,
   ChevronRightIcon,
   CloseIcon,
+  CloudIcon,
   DotIcon,
+  DownloadIcon,
   EditIcon,
   EmailIcon,
   ErrorIcon,
   EuroIcon,
+  EyeIcon,
+  EyeSlashIcon,
+  FilterIcon,
   FolderIcon,
   GridIcon,
+  HeartIcon,
+  HouseIcon,
   InfoIcon,
   LayersIcon,
+  LoginIcon,
+  LogoutIcon,
+  MenuIcon,
   PenIcon,
+  PlusIcon,
+  SearchIcon,
+  SettingsIcon,
   ShieldIcon,
+  SortIcon,
   SpinnerIcon,
   TickIcon,
   TrashIcon,
+  UploadIcon,
   UserIcon,
   UsersIcon,
+  VerifiedIcon,
   WarningIcon,
 };
