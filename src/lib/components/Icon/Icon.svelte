0b7ddf6a7bc3ec2cf2aa2d50b987ac93a9cb7236
Leonardo <leonardodipace@gmail.com>
2023-10-09 13:09:16 +0200

    Added icon variants

--- src/lib/components/Icon/Icon.svelte
+++ src/lib/components/Icon/Icon.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
   import { cn } from "$lib/utils/misc";
   import type { HTMLAttributes } from "svelte/elements";
+  import { tv, type VariantProps } from "tailwind-variants";
 
-  type $$Props = HTMLAttributes<HTMLDivElement>;
+  type $$Props = HTMLAttributes<HTMLDivElement> & {
+    variant?: VariantProps<typeof iconVariants>["variant"];
+  };
 
   export { className as class };
+  export let variant: $$Props["variant"] = "default";
+
+  const iconVariants = tv({
+    variants: {
+      variant: {
+        default: cn`
+          bg-transparent
+          text-inherit
+        `,
+        success: cn`
+          bg-primary/5
+          text-primary
+        `,
+        info: cn`
+          bg-muted/5
+          text-muted
+        `,
+        warning: cn`
+          bg-warning/5
+          text-warning
+        `,
+        error: cn`
+          bg-error/5
+          text-error
+        `,
+      },
+    },
+  });
 
   let className = "";
 </script>
@@ -13,6 +44,7 @@
   class={cn`
     w-5
     h-5
+    ${iconVariants({ variant })}
     ${className}
   `}
   {...$$restProps}
