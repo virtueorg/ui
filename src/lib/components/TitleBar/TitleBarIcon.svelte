fd37d92b958da878f21bd0f2f3cc9b0222172877
Leonardo <leonardodipace@gmail.com>
2023-10-09 10:43:49 +0200

    Added TitleBarIcon

---
+++ src/lib/components/TitleBar/TitleBarIcon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { cn } from "$lib/utils/misc";
+  import type { HTMLAttributes } from "svelte/elements";
+
+  type $$Props = HTMLAttributes<HTMLDivElement>;
+
+  export { className as class };
+
+  let className = "";
+</script>
+
+<div
+  class={cn`
+    w-11
+    h-11
+    flex
+    items-center
+    justify-center
+    rounded-lg
+    bg-muted/5
+    ${className}
+  `}
+  {...$$restProps}
+>
+  <slot />
+</div>
