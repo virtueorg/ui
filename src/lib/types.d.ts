2deeca6b8803c10a61303823ac469fcf6f82f353
Leonardo <leonardodipace@gmail.com>
2023-10-18 15:55:54 +0200

    Added IHeadingTag type

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,5 +1,7 @@
 export type IVariant = "default" | "success" | "info" | "warning" | "error";
 
+export type IHeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
 export type IAlert = {
   id?: string;
   icon?: ConstructorOfATypedSvelteComponent;
