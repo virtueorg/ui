f0c81e97d5d8efece8f663496aca32c7ffd9a42b
Leonardo <leonardodipace@gmail.com>
2023-10-18 16:04:17 +0200

    Simplified Icon component

--- src/lib/components/Icon/Icon.svelte
+++ src/lib/components/Icon/Icon.svelte
@@ -1,49 +1,46 @@
 <script lang="ts">
+  import type { IVariant } from "$lib/types";
   import { cn } from "$lib/utils/misc";
   import type { HTMLAttributes } from "svelte/elements";
-  import { tv, type VariantProps } from "tailwind-variants";
 
   type $$Props = HTMLAttributes<HTMLDivElement> & {
-    variant?: VariantProps<typeof iconVariants>["variant"];
+    variant?: IVariant;
   };
 
   export { className as class };
   export let variant: $$Props["variant"] = "default";
 
-  const iconVariants = tv({
-    variants: {
-      variant: {
-        default: cn`
-          text-inherit
-        `,
-        success: cn`
-          text-primary
-        `,
-        info: cn`
-          text-muted
-        `,
-        warning: cn`
-          text-warning
-        `,
-        error: cn`
-          text-error
-        `,
-      },
-    },
-  });
-
   let className = "";
 </script>
 
-<div
-  class={cn`
-    w-5
-    h-5
-    shrink-0
-    ${iconVariants({ variant })}
-    ${className}
-  `}
-  {...$$restProps}
->
+<div class={cn("Icon", variant, className)} {...$$restProps}>
   <slot />
 </div>
+
+<style lang="postcss">
+  .Icon {
+    @apply w-5;
+    @apply h-5;
+    @apply shrink-0;
+  }
+
+  .default {
+    @apply text-inherit;
+  }
+
+  .success {
+    @apply text-primary;
+  }
+
+  .info {
+    @apply text-muted;
+  }
+
+  .warning {
+    @apply text-warning;
+  }
+
+  .error {
+    @apply text-error;
+  }
+</style>
