56fd9e39f4a2fa9b00c3741c8ef7bcbced2d0577
Leonardo <leonardodipace@gmail.com>
2023-10-18 09:26:34 +0200

    Added temp example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,31 @@
-<script>
+<script lang="ts">
   import Avatar from "$lib/components/Avatar/Avatar.svelte";
+  import AvatarFallback from "$lib/components/Avatar/AvatarFallback.svelte";
+  import AvatarImage from "$lib/components/Avatar/AvatarImage.svelte";
+  import Icon from "$lib/components/Icon/Icon.svelte";
+  import Input from "$lib/components/Input/Input.svelte";
+  import InputField from "$lib/components/Input/InputField.svelte";
+  import InputLabel from "$lib/components/Input/InputLabel.svelte";
+  import InputTrigger from "$lib/components/Input/InputTrigger.svelte";
+  import UserIcon from "$lib/icons/UserIcon.svelte";
+
+  let username = "leonardodipace";
 </script>
 
-<Avatar src="favicon.webp" delayMs={1000} />
+<div class="p-5 flex flex-col gap-5">
+  <Input>
+    <InputTrigger>
+      <InputLabel>Username</InputLabel>
+      <InputField type="text" placeholder="Es. leonardodipace" hasLabel bind:value={username} />
+    </InputTrigger>
+  </Input>
+
+  <Avatar>
+    <AvatarImage src="https://www.github.com/{username}.png" alt={username} />
+    <AvatarFallback>
+      <Icon>
+        <UserIcon />
+      </Icon>
+    </AvatarFallback>
+  </Avatar>
+</div>
