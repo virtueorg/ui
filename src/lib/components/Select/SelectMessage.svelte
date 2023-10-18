bb9c0069d0c5a8d34285dcdcea80ae229cb6aa30
Leonardo <leonardodipace@gmail.com>
2023-10-18 16:34:56 +0200

    Removed variants

--- src/lib/components/Select/SelectMessage.svelte
+++ src/lib/components/Select/SelectMessage.svelte
@@ -1,46 +1,44 @@
 <script lang="ts">
+  import type { IVariant } from "$lib/types";
   import { cn } from "$lib/utils/misc";
   import type { HTMLAttributes } from "svelte/elements";
-  import { tv, type VariantProps } from "tailwind-variants";
 
   type $$Props = HTMLAttributes<HTMLDivElement> & {
-    variant?: VariantProps<typeof selectMessageVariants>["variant"];
+    variant?: IVariant;
   };
 
   export { className as class };
   export let variant: $$Props["variant"] = "default";
 
-  const selectMessageVariants = tv({
-    variants: {
-      variant: {
-        default: cn`
-          text-foreground
-        `,
-        success: cn`
-          text-primary
-        `,
-        info: cn`
-          text-muted
-        `,
-        warning: cn`
-          text-warning
-        `,
-        error: cn`
-          text-error
-        `,
-      },
-    },
-  });
-
   let className = "";
 </script>
 
-<div class={cn("SelectMessage", selectMessageVariants({ variant }), className)} {...$$restProps}>
+<div class={cn("SelectMessage", variant, className)} {...$$restProps}>
   <slot />
 </div>
 
 <style lang="postcss">
   .SelectMessage {
     @apply text-sm;
+  }
+
+  .default {
+    @apply text-foreground;
+  }
+
+  .success {
+    @apply text-primary;
+  }
+
+  .info {
+    @apply text-muted;
+  }
+
+  .warning {
+    @apply text-warning;
+  }
+
+  .error {
+    @apply text-error;
   }
 </style>
