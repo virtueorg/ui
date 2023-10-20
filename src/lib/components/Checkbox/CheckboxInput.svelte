90e66e301d4ab19bec48cc9fa5d87e7180dd8618
Leonardo <leonardodipace@gmail.com>
2023-10-20 15:24:32 +0200

    Added missing builder

--- src/lib/components/Checkbox/CheckboxInput.svelte
+++ src/lib/components/Checkbox/CheckboxInput.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { cn } from "$lib/utils/misc";
+  import { melt } from "@melt-ui/svelte";
   import type { HTMLInputAttributes } from "svelte/elements";
   import ctx from "./ctx";
 
@@ -12,13 +13,20 @@
   let className = "";
   export let asChild: $$Props["asChild"] = false;
 
-  const { helpers, options } = ctx.get();
+  const { elements, helpers, options } = ctx.get();
+  const { input } = elements;
   const { isChecked } = helpers;
   const { disabled } = options;
 </script>
 
 {#if asChild}
-  <slot isChecked={$isChecked} disabled={$disabled} />
+  <slot builder={$input} isChecked={$isChecked} disabled={$disabled} />
 {:else}
-  <input hidden class={cn(className)} value={$isChecked} disabled={$disabled} {...$$restProps} />
+  <input
+    class={cn(className)}
+    checked={$isChecked}
+    disabled={$disabled}
+    use:melt={$input}
+    {...$$restProps}
+  />
 {/if}
