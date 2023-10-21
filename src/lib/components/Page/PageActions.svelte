281151af9c872105acc1354fc60f745510adafa0
Leonardo <leonardodipace@gmail.com>
2023-10-21 11:26:05 +0200

    Added Page component

---
+++ src/lib/components/Page/PageActions.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { cn } from "$lib/utils";
+  import type { HTMLAttributes } from "svelte/elements";
+  import { tv } from "tailwind-variants";
+
+  type $$Props = HTMLAttributes<HTMLDivElement>;
+
+  export { className as class };
+
+  let className = "";
+
+  const style = tv({
+    base: cn`
+      flex
+      items-center
+      gap-2
+    `,
+  });
+</script>
+
+<div class={cn(style.base, className)} {...$$restProps}>
+  <slot />
+</div>
---
+++ src/lib/components/Page/index.ts
@@ -0,0 +1,6 @@
+import Page from "./Page.svelte";
+import PageActions from "./PageActions.svelte";
+import PageContent from "./PageContent.svelte";
+import PageHeading from "./PageHeading.svelte";
+
+export { Page, PageActions, PageContent, PageHeading };
