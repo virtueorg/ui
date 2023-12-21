6fb3e9d3e55222ba0dd959137c034acf7debf6e3
Leonardo <leonardodipace@gmail.com>
2023-12-21 13:00:20 +0100

    Added scale transition

--- src/lib/components/Tooltip/TooltipContent.svelte
+++ src/lib/components/Tooltip/TooltipContent.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { cn } from "$lib"
+  import { TRANSITION_SCALE, cn } from "$lib"
   import type { AsChild } from "$lib/types"
   import { melt } from "@melt-ui/svelte"
   import type { HTMLAttributes } from "svelte/elements"
+  import { scale } from "svelte/transition"
   import { tv } from "tailwind-variants"
   import ctx from "./ctx"
 
@@ -21,14 +22,22 @@
     `,
   })
 
-  const { elements } = ctx.get()
+  const { elements, states } = ctx.get()
+  const { open } = states
   const { content } = elements
 </script>
 
-{#if asChild}
-  <slot builder={$content} />
-{:else}
-  <div class={cn(style.base, className)} use:melt={$content} {...$$restProps}>
-    <slot />
-  </div>
+{#if $open}
+  {#if asChild}
+    <slot builder={$content} />
+  {:else}
+    <div
+      class={cn(style.base, className)}
+      use:melt={$content}
+      {...$$restProps}
+      transition:scale|global={TRANSITION_SCALE}
+    >
+      <slot />
+    </div>
+  {/if}
 {/if}
