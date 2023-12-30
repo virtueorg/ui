6cfc54128f0779384961834e0ecb2acf129d450b
Leonardo <leonardodipace@gmail.com>
2023-12-30 10:56:57 +0100

    Enhanced alerts API

--- src/lib/stores/alerts.ts
+++ src/lib/stores/alerts.ts
@@ -3,9 +3,9 @@ import type { IAlert } from "$lib/types"
 import { nanoid } from "nanoid"
 import { writable, type Writable } from "svelte/store"
 
-export const alerts: Writable<IAlert[]> = writable([])
+const alerts: Writable<IAlert[]> = writable([])
 
-export const addAlert = (alert: IAlert) => {
+const add = (alert: IAlert) => {
   if (!alert.id) {
     alert.id = nanoid()
   }
@@ -15,12 +15,18 @@ export const addAlert = (alert: IAlert) => {
   })
 
   setTimeout(() => {
-    removeAlert(alert.id)
+    remove(alert.id)
   }, ALERT_TIMEOUT)
 }
 
-export const removeAlert = (id: IAlert["id"]) => {
+const remove = (id: IAlert["id"]) => {
   alerts.update($alerts => {
     return $alerts.filter(alert => alert.id !== id)
   })
+}
+
+export default {
+  ...alerts,
+  add,
+  remove,
 }
