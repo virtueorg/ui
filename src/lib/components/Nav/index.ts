2b58ddc9ae68a5b6142d1b6d97733398ee85b44e
Leonardo <leonardodipace@gmail.com>
2024-02-18 02:10:02 +0100

    Removed Nav

--- src/lib/components/Nav/index.ts
+++
@@ -1,8 +0,0 @@
-import NavBrand from "./NavBrand.svelte"
-import NavFooter from "./NavFooter.svelte"
-import NavLi from "./NavLi.svelte"
-import NavLink from "./NavLink.svelte"
-import Nav from "./NavRoot.svelte"
-import NavUl from "./NavUl.svelte"
-
-export { Nav, NavBrand, NavFooter, NavLi, NavLink, NavUl }
