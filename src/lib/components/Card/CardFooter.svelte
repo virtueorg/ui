cb1a9ae95f64d1fa5f8d1550e259841214610b25
Leonardo <leonardodipace@gmail.com>
2024-02-18 01:59:40 +0100

    Minor design improvements

--- src/lib/components/Card/CardFooter.svelte
+++ src/lib/components/Card/CardFooter.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-  import { cn } from "$lib"
+  import { cn } from "$lib/index.js"
+  import type { AsChild } from "$lib/types.js"
   import type { HTMLAttributes } from "svelte/elements"
   import { tv } from "tailwind-variants"
 
-  type $$Props = HTMLAttributes<HTMLDivElement>
+  type $$Props = HTMLAttributes<HTMLDivElement> & AsChild
 
+  export let asChild: $$Props["asChild"] = false
   export { className as class }
 
   let className = ""
 
   const style = tv({
     base: cn`
-      p-5
+      p-4
     `,
   })
 </script>
 
-<div class={cn(style.base, className)} {...$$restProps}>
+{#if asChild}
   <slot />
-</div>
+{:else}
+  <footer class={cn(style.base, className)} {...$$restProps}>
+    <slot />
+  </footer>
+{/if}
