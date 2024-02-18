ae3e03bdd3b5ce6c20c6265b65e251f015788c5a
Leonardo <leonardodipace@gmail.com>
2024-02-18 01:58:30 +0100

    Renamed BadgeRoot to Badge

--- src/lib/components/Badge/Badge.svelte
+++ src/lib/components/Badge/Badge.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
-  import { cn } from "$lib"
-  import type { IVariant } from "$lib/types"
+  import { cn } from "$lib/index.js"
+  import type { AsChild, Variant } from "$lib/types.js"
   import type { HTMLAttributes } from "svelte/elements"
   import { tv } from "tailwind-variants"
 
-  type $$Props = HTMLAttributes<HTMLSpanElement> & {
-    variant?: IVariant
-  }
+  type $$Props = HTMLAttributes<HTMLDivElement> &
+    AsChild & {
+      variant?: Variant
+    }
 
-  export { className as class }
+  export let asChild: $$Props["asChild"] = false
   export let variant: $$Props["variant"] = "default"
+  export { className as class }
 
   let className = ""
 
   const style = tv({
     base: cn`
-      inline-flex
-      items-center
-      gap-2
-      py-2
-      px-3
-      rounded-lg
+     inline-flex
+     p-3
+     rounded-xl
+     border
     `,
     variants: {
       variant: {
         default: cn`
-          bg-transparent
+          border-muted/5
+          bg-muted/5
+          text-muted
         `,
         success: cn`
+          border-primary/5
           bg-primary/5
           text-primary
         `,
         info: cn`
+          border-muted/5
           bg-muted/5
           text-muted
         `,
         warning: cn`
+          border-warning/5
           bg-warning/5
           text-warning
         `,
         error: cn`
+          border-error/5
           bg-error/5
           text-error
         `,
@@ -48,6 +54,10 @@
   })
 </script>
 
-<span class={cn(style.base, style({ variant }), className)} {...$$restProps}>
+{#if asChild}
   <slot />
-</span>
+{:else}
+  <div class={cn(style.base, style({ variant }), className)} {...$$restProps}>
+    <slot />
+  </div>
+{/if}
