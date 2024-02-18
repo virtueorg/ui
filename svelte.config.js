a342af6fecd10ef5216428e842614d98cf561e57
Leonardo <leonardodipace@gmail.com>
2024-02-18 01:42:19 +0100

    Updated sequence import

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-import { preprocessMeltUI } from "@melt-ui/pp"
+import { preprocessMeltUI, sequence } from "@melt-ui/pp"
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import sequence from "svelte-sequential-preprocessor"
 
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
