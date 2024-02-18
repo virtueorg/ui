c7c3af61ec30f6566b4a72cd8e39c3650f942e6c
Leonardo <leonardodipace@gmail.com>
2024-02-18 01:57:57 +0100

    Renamed AvatarRoot to Avatar

--- src/lib/components/Avatar/Avatar.svelte
+++ src/lib/components/Avatar/Avatar.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { cn } from "$lib"
-  import type { AsChild } from "$lib/types"
-  import type { CreateAvatarProps } from "@melt-ui/svelte"
+  import { cn } from "$lib/index.js"
+  import type { AsChild } from "$lib/types.js"
+  import { type CreateAvatarProps } from "@melt-ui/svelte"
   import { tv } from "tailwind-variants"
-  import ctx from "./ctx"
+  import ctx from "./ctx.js"
 
   type $$Props = Omit<CreateAvatarProps, "src"> & AsChild
 
@@ -14,21 +14,20 @@
 
   const style = tv({
     base: cn`
-      w-11
-      h-11
-      flex
+      w-full
+      h-full
+      rounded-xl
+      overflow-hidden
+      inline-flex
       items-center
       justify-center
+      border
+      border-muted/5
       bg-muted/5
-      rounded-lg
-      overflow-hidden
     `,
   })
 
-  ctx.create({
-    ...$$restProps,
-    src: "",
-  })
+  ctx.set({ src: "", ...$$restProps })
 </script>
 
 {#if asChild}
