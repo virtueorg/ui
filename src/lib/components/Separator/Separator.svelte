5d6db4555bf5d51c0ff5dd0253dff8f7a22f01c2
Leonardo <leonardodipace@gmail.com>
2024-02-18 02:10:57 +0100

    Added Separator components

---
+++ src/lib/components/Separator/Separator.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { cn } from "$lib/index.js"
+  import type { AsChild } from "$lib/types.js"
+  import { melt, type CreateSeparatorProps } from "@melt-ui/svelte"
+  import { tv } from "tailwind-variants"
+  import ctx from "./ctx.js"
+
+  type $$Props = CreateSeparatorProps & AsChild
+
+  export let asChild: $$Props["asChild"] = false
+  export let orientation: $$Props["orientation"] = "horizontal"
+  export { className as class }
+
+  let className = ""
+
+  const style = tv({
+    base: cn`
+      bg-muted/5
+      shrink-0
+    `,
+    variants: {
+      orientation: {
+        horizontal: cn`
+          w-full
+          h-px
+        `,
+        vertical: cn`
+          w-px
+          h-full
+        `,
+      },
+    },
+  })
+
+  const { elements } = ctx.set({ ...$$restProps, orientation })
+  const { root } = elements
+
+  $: builder = $root
+</script>
+
+{#if asChild}
+  <slot {builder} />
+{:else}
+  <div class={cn(style.base, style({ orientation }), className)} use:melt={builder} {...$$restProps} />
+{/if}
