2a8e4bfdaab547a5d0872755631694cf57c297da
Leonardo <leonardodipace@gmail.com>
2024-02-18 02:10:42 +0100

    Minor design improvements

--- src/lib/components/Section/Section.svelte
+++ src/lib/components/Section/Section.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
   import { cn } from "$lib/index.js"
-  import type { AsChild, HeadingLevel } from "$lib/types.js"
+  import type { AsChild } from "$lib/types.js"
   import type { HTMLAttributes } from "svelte/elements"
   import { tv } from "tailwind-variants"
 
-  type $$Props = HTMLAttributes<HTMLHeadingElement> &
-    AsChild & {
-      level?: HeadingLevel
-    }
+  type $$Props = HTMLAttributes<HTMLDivElement> & AsChild
 
   export let asChild: $$Props["asChild"] = false
-  export let level: $$Props["level"] = "h3"
   export { className as class }
 
   let className = ""
 
   const style = tv({
     base: cn`
-      text-sm
-      text-muted
+      flex
+      flex-col
+      gap-2
     `,
   })
 </script>
@@ -26,7 +23,7 @@
 {#if asChild}
   <slot />
 {:else}
-  <svelte:element this={level} class={cn(style.base, className)} {...$$restProps}>
+  <section class={cn(style.base, className)} {...$$restProps}>
     <slot />
-  </svelte:element>
+  </section>
 {/if}
--- src/lib/components/Section/index.ts
+++ src/lib/components/Section/index.ts
@@ -1,5 +1,3 @@
-import SectionContent from "./SectionContent.svelte"
-import Section from "./SectionRoot.svelte"
-import SectionTitle from "./SectionTitle.svelte"
-
-export { Section, SectionContent, SectionTitle }
+export { default as Root } from "./Section.svelte"
+export { default as Content } from "./SectionContent.svelte"
+export { default as Title } from "./SectionTitle.svelte"
