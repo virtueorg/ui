c8c93e68189fc8b08eedd8969ad45176168e831a
Leonardo <leonardodipace@gmail.com>
2024-02-18 03:00:35 +0100

    Exported missing components

--- src/lib/components/Card/index.ts
+++ src/lib/components/Card/index.ts
@@ -1,4 +1,6 @@
 export { default as Root } from "./Card.svelte"
 export { default as Content } from "./CardContent.svelte"
+export { default as Description } from "./CardDescription.svelte"
 export { default as Footer } from "./CardFooter.svelte"
 export { default as Header } from "./CardHeader.svelte"
+export { default as Title } from "./CardTitle.svelte"
