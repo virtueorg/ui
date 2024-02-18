65297308587af813ef6c721a493b4b68dbf7c3bd
Leonardo <leonardodipace@gmail.com>
2024-02-18 02:07:08 +0100

    Renamed ContextMenuRadioIndicator to ContextMenuRadioItemIndicator

---
+++ src/lib/components/ContextMenu/ContextMenuRadioItemIndicator.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { cn } from "$lib/index.js"
+  import type { AsChild } from "$lib/types.js"
+  import type { HTMLAttributes } from "svelte/elements"
+  import ctx from "./ctx.js"
+
+  type $$Props = HTMLAttributes<HTMLDivElement> & AsChild
+
+  export let asChild: $$Props["asChild"] = false
+  export { className as class }
+
+  let className = ""
+
+  const { value } = ctx.getRadioItem()
+  const { helpers } = ctx.getRadioGroup()
+  const { isChecked } = helpers
+</script>
+
+{#if asChild}
+  <slot isChecked={$isChecked(value)} />
+{:else}
+  <div class={cn(className)} {...$$restProps}>
+    <slot isChecked={$isChecked(value)} />
+  </div>
+{/if}
