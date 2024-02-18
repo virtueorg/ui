cdc754e83a9fec9baf227aca57a6466857483d86
Leonardo <leonardodipace@gmail.com>
2024-02-18 01:50:43 +0100

    Removed icons

--- src/lib/icons/MenuIcon.svelte
+++
@@ -1,9 +0,0 @@
-<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path
-    d="M2.5 10H17.5M2.5 5H17.5M2.5 15H17.5"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-</svg>
--- src/lib/icons/PlusIcon.svelte
+++
@@ -1,9 +0,0 @@
-<svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path
-    d="M9.99984 4.16675V15.8334M4.1665 10.0001H15.8332"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  />
-</svg>
