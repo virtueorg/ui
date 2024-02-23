36b212856a8d9184126cb2be0f293c05dd07210a
Leonardo <leonardodipace@gmail.com>
2024-02-23 10:02:37 +0100

    Added ExtendedVariant type

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -2,6 +2,8 @@ import type { Action } from "svelte/action"
 
 export type Variant = "default" | "success" | "info" | "warning" | "error"
 
+export type ExtendedVariant = Variant | "successFilled" | "infoFilled" | "warningFilled" | "errorFilled"
+
 export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
 
 export type AsChild = {
